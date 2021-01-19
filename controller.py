b624823430b520765a21a5f946aaa678aed1ba6c
Maximilian Werner <maximilian.werner96@gmail.com>
2021-01-19 10:07:43 +0100

    Add enable buttons

--- controller.py
+++ controller.py
@@ -6,17 +6,25 @@ class DelayChannel:
     def __init__(self, channel, index, base_row, base_col):
         self.channel = channel
         self.index = index
-        self.enable = False
+
         self.scale_vol = Scale(master=root, from_=4, to=0, resolution = 0.01, command=handleVolume(index, 0, self), length = 200, label = 'vol', troughcolor='green')
         self.scale_vol.grid(row = base_row, column = base_col, padx=0, pady=5)
         self.scale_vol.set(0)
+
         self.scale_del = Scale(master=root, from_=6, to=0, resolution = 0.01, command=handleDelay(index, 0, self), length = 200, label = 'del')
         self.scale_del.grid(row = base_row, column = base_col+1, padx=0, pady=5)
         self.scale_del.set(0)
+
         self.label = Label(master=root, text=self.getName())
         self.label.grid(row = base_row+1, column = base_col, padx=0, pady=5)
-        self.checkbox = Checkbutton(master=root, text='enable', var=self.enable)
+
+        self.enable = BooleanVar()
+        self.checkbox = Checkbutton(master=root, text='enable', var=self.enable, command=updateCheckbox)
         self.checkbox.grid(row = base_row+1, column = base_col+1, padx=0, pady=5)
+        if channel == 'l':
+            checkboxes_left.append(self.enable)
+        else:
+            checkboxes_right.append(self.enable)
 
     def getName(self):
         return self.channel + str(self.index)
@@ -48,6 +56,16 @@ def handleVolume(module, register, slider):
 def handleDelay(module, register, slider):
     return lambda val : sendDelay(module, register, slider.scale_del)
 
+def updateCheckbox():
+    val = 0
+    for count, box in enumerate(checkboxes_left):
+        val |= box.get() << count
+    sendVal(0, 0, val)
+    val = 0
+    for count, box in enumerate(checkboxes_right):
+        val |= box.get() << count
+    sendVal(0, 1, val)
+
 def openSerial():
     if ser.is_open:
         print('Closing port: ', ser.port)
@@ -65,14 +83,26 @@ root.title('Register Interface')
 
 ser = serial.Serial()
 
-# faders
+# enable-boxes
+checkboxes_left = []
+checkboxes_right = []
+
+# volume faders
 faders = []
 fadernames = ['vol_l', 'vol_r']
 for i in range(2):
     faders.append(Scale(master=root, from_=4, to=0, resolution = 0.01, command=handleSlider(i), length = 200, label = fadernames[i], troughcolor='blue'))
     faders[i].grid(row = i*2, column = 1, padx=0, pady=5)
     faders[i].set(1)
+    enable = BooleanVar()
+    checkbox = Checkbutton(master=root, text='enable_del', var=enable, command=updateCheckbox)
+    checkbox.grid(row = i*2+1, column = 1, padx=0, pady=5)
+    if fadernames[i] == 'vol_l':
+        checkboxes_left.append(enable)
+    else:
+        checkboxes_right.append(enable)
 
+# delay controls
 delays = []
 for i in range(7):
     delays.append(DelayChannel('l', i, 0, i*2+3))
